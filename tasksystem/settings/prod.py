0edd807929d21c3d7a37018777778cc2e96dfaf0
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-09-13 17:03:30 +0200

    config db properly

--- tasksystem/settings/prod.py
+++ tasksystem/settings/prod.py
@@ -5,7 +5,7 @@ import dj_database_url
 DEBUG = False
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = db_from_env
 
 # websockets
 CHANNEL_LAYERS['default'] = {
