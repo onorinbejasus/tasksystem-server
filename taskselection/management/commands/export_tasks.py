6b7fec282ef7550d39e7efa930f3c4f51862e9bf
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-09-22 10:52:24 +0200

    script to export tasks with SVs to a csv

---
+++ taskselection/management/commands/export_tasks.py
@@ -0,0 +1,40 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
+from taskselection.models import Task
+import sys
+import csv
+
+User = get_user_model()
+
+EXPORT_FIELDS = [
+  'desc',
+  'code',
+  'date',
+  'starttime',
+  'endtime',
+  'firstname',
+  'lastname',
+  'email'
+]
+
+class Command(BaseCommand):
+  help = 'Creates new tasks in the db for the given csv file'
+
+  def add_arguments(self, parser):
+    parser.add_argument('tasks', type=str)
+
+  def handle(self, *args, **options):
+    with open(options['tasks'], 'w') as csvfile:
+      csvwriter = csv.writer(csvfile, delimiter=';')
+      csvwriter.writerow(EXPORT_FIELDS)
+      # if this doesn't work the delimiter might be wrong
+      tasks = Task.objects.all()
+      #.values_list(EXPORT_FIELDS, flat=True)
+      for task in tasks:
+        row = [task.desc, task.code, task.date, task.starttime, task.endtime]
+        if task.sv:
+          row = row + [task.sv.first_name, task.sv.last_name, task.sv.email]
+        csvwriter.writerow(row)
+
+
+
