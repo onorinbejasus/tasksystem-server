5082c602d399246ce9dd38f0be90b0c7e54d37c8
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-09-28 18:39:10 +0200

    script to export sv schedules

---
+++ taskselection/management/commands/print_schedules.py
@@ -0,0 +1,91 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
+from taskselection.models import Task
+import sys
+from os import path
+import csv
+
+User = get_user_model()
+
+EXPORT_FIELDS = [
+  ('code', '4em'),
+  ('desc', '20em'),
+  ('starttime', '5em'),
+  ('endtime', '5em'),
+  ('location', '10em')
+]
+
+PREAMBLE = """
+\\documentclass[12pt]{article}
+
+\\usepackage[landscape,margin=0.5in]{geometry}
+\\usepackage{longtable}
+
+\\begin{document}
+
+"""
+
+SV_SECTION_START = """
+\\section*{%s}
+
+\\renewcommand\\arraystretch{2.1}
+\\begin{center}
+\\begin{longtable}{%s}
+\\hline
+%s \\\\
+\\hline
+\\endhead
+"""
+
+SV_SECTION_END = """
+\\hline
+\\end{longtable}
+\\end{center}
+"""
+
+POSTAMBLE = """
+\\end{document}
+"""
+
+def sanitize_latex(s):
+  return s.replace("&", "\\&")
+
+def format_head(sv, fields):
+  frmt = "|" + "|".join(["p{"+f[1]+"}" for f in fields]) + "|"
+  flds = ["\\textbf{%s}" % (f[0]) for f in fields]
+  flds = " & ".join(flds)
+  name = sv.first_name + ' ' + sv.last_name
+  return SV_SECTION_START % (name, frmt, flds)
+
+def format_line(task):
+  row = [task.code, task.desc, task.starttime, task.endtime, task.location]
+  row = [str(f) for f in row]
+  row = [sanitize_latex(f) for f in row]
+  return " & ".join(row) + " \\\\"
+
+class Command(BaseCommand):
+  help = 'Prints the task lists for each SV'
+
+  def add_arguments(self, parser):
+    parser.add_argument('file', type=str)
+
+  def handle(self, *args, **options):
+    with open(options['file'], 'w') as output:
+      print(PREAMBLE, file=output)
+
+      # make a page for each SV
+      svs = User.objects.order_by('last_name', 'first_name').all()
+      for sv in svs:
+        tasks = Task.objects.filter(sv=sv).order_by('date', 'starttime')
+        if len(tasks) > 0:
+          print_sv_tasks(sv, tasks, output)
+          print("\\clearpage", file=output)
+      print(POSTAMBLE, file=output)
+      
+def print_sv_tasks(sv, tasks, output):
+  print(format_head(sv, EXPORT_FIELDS), file=output)
+  for task in tasks:
+    print(format_line(task), file=output)
+    print("\\hline", file=output)
+  print(SV_SECTION_END, file=output)
+
